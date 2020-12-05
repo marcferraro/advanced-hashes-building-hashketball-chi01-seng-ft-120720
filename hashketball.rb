56d6430bf1feae4b39d0d6b7d645dd4739ac67fa
Marc Ferraro <marc.ferraro1@gmail.com>
2020-12-05 17:49:43 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -7,7 +7,7 @@ def game_hash
       :team_name => "Brooklyn Nets",
       :colors => ["Black","White"],
       :players => [{
-        :player_name => "Alan Anderson", :number =>, :shoe =>, :points =>, :rebounds =>, :assists =>, :steals =>, :blocks =>, :slam_dunks =>
+        :player_name => "Alan Anderson", :number => 0, :shoe => 16, :points => 22, :rebounds => 12, :assists => 12, :steals => 3, :blocks => 1, :slam_dunks => 1
       },{
         :
       }]
@@ -26,3 +26,5 @@ def game_hash
   
   return game_data
 end
+
+#:player_name => "Alan Anderson", :number =>, :shoe =>, :points =>, :rebounds =>, :assists =>, :steals =>, :blocks =>, :slam_dunks =>
