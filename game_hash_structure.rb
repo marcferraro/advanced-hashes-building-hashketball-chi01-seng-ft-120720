970e8e20014534a858a9a1ed00a26daa7ed877eb
Marc Ferraro <marc.ferraro1@gmail.com>
2020-12-05 18:07:32 +0000

    Done.

---
+++ game_hash_structure.rb
@@ -0,0 +1,18 @@
+game_data = {
+  :home =>{
+  :team_name =>"team_name",
+  :colors =>["colors1","colors2"],
+  :players =>[{
+    :player_name => "",
+    :number => 0,
+    :shoe => 0,
+    :points => 0,
+    :rebounds => 0,
+    :assists => 0,
+    :steals => 0,
+    :blocks => 0,
+    :slam_dunks => 0
+  }{}{}]
+  },
+  :away =>{}
+}
