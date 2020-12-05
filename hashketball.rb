cbe3cb7fdf30b608dba0d75f1855f0d9bec08e06
Marc Ferraro <marc.ferraro1@gmail.com>
2020-12-05 17:47:06 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,28 @@
-# Write your code here!
+require 'pry'
+
+def game_hash
+  
+  game_data = {
+    :home => {
+      :team_name => "Brooklyn Nets",
+      :colors => ["Black","White"],
+      :players => [{
+        :player_name => "Alan Anderson",
+      },{
+        :
+      }]
+    },
+    :away => {
+      :team_name => "Charlotte Hornets",
+      :colors => ["Turquoise, Purple"],
+      :players => [{},{}]
+    }
+    
+  }
+  
+  
+  
+  
+  
+  return game_data
+end
